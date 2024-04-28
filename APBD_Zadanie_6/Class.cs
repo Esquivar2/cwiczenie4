dd97ef85d4738b7441379f2d5eaa469b4282f6e1
s26294 <s26294@pjwstk.edu.pl>
2024-04-28 20:26:32 +0200

    to co zdazylem na zajeciach -_-

---
+++ APBD_Zadanie_6/Class.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APBD_Zadanie_6
+{
+    [ApiController]
+    [Route("api/warehouse2")]
+    public class Class : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> AddProductToWarehouse
+    }
+}
---
+++ APBD_Zadanie_6/Controllers/WarehousesController.cs
@@ -0,0 +1,26 @@
+﻿using APBD_Task_6.Models;
+using Microsoft.AspNetCore.Mvc;
+using Zadanie5.Services;
+
+namespace Zadanie5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarehousesController : ControllerBase
+    {
+        private readonly IWarehouseService _warehouseService;
+
+        public WarehousesController(IWarehouseService warehouseService)
+        {
+            _warehouseService = warehouseService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(ProductWarehouse product)
+        {
+            int idProduct
+            _warehouseService.AddProduct(product);
+            return Ok();
+        }
+    }
+}
